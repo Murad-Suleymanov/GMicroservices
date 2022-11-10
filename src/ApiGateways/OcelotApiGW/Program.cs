8c116541780b32d1db2edf611977204971724937
muradsuleymanov868 <muradsuleymanov868@gmail.com>
2022-11-10 11:06:23 +0400

    Add ocelot json and change docker compose

--- src/ApiGateways/OcelotApiGW/Program.cs
+++ src/ApiGateways/OcelotApiGW/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Host.
+    ConfigureAppConfiguration((hostingContext, config) =>
+    {
+       config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+    })
+    .ConfigureLogging((hostingContext, loggingbuilder) =>
+    {
+        loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        loggingbuilder.AddConsole();
+        loggingbuilder.AddDebug();
+    });
+
+builder.Services.AddOcelot();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +39,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
